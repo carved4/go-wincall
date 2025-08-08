07e5698dbbd028ef0c81d986dced7bbd889231ee
carved4 <osmith26@adrian.edu>
2025-08-08 01:04:09 -0700

    hash fix

--- pkg/obf/obf.go
+++ pkg/obf/obf.go
@@ -7,6 +7,10 @@ import (
 	"encoding/binary"
 	"time"
 	"unsafe"
+	"crypto/sha256"
+	"crypto/rand"
+	"os"
+	"runtime"
 )
 
 var (
@@ -14,20 +18,25 @@ var (
 	keyInitOnce sync.Once
 )
 
-// this is not meant to be cryptographically secure 
+// this is now cryptographically secure 
 func generateRuntimeKey() []byte {
-	now := time.Now()
-	seed := uint64(now.UnixNano())
-	
-	seed ^= uint64(now.Unix()) << 32
-	seed ^= uint64(now.Nanosecond()) << 16
-	seed ^= uint64(uintptr(unsafe.Pointer(&seed)))
-	seed ^= uint64(uintptr(unsafe.Pointer(&now)))
-	
 	key := make([]byte, 64)
-	for i := 0; i < len(key); i++ {
-		seed = seed*1103515245 + 12345
-		key[i] = byte(seed >> 16)
+	n, err := rand.Read(key)
+	if err != nil || n != len(key) {
+		now := time.Now()
+		seed := uint64(now.UnixNano())
+		
+		seed ^= uint64(now.Unix()) << 32
+		seed ^= uint64(now.Nanosecond()) << 16
+		seed ^= uint64(uintptr(unsafe.Pointer(&seed)))
+		seed ^= uint64(uintptr(unsafe.Pointer(&now)))
+		seed ^= uint64(os.Getpid()) << 24
+		seed ^= uint64(runtime.NumGoroutine()) << 8
+		
+		for i := 0; i < len(key); i++ {
+			seed = seed*1103515245 + 12345
+			key[i] = byte(seed >> 16)
+		}
 	}
 	
 	return key
@@ -73,41 +82,61 @@ func DecodeUintptr(encoded []byte) uintptr {
 	}
 	return uintptr(binary.LittleEndian.Uint64(decoded))
 }
-
 var (
-	hashSeed     uint32
+	hashSeed     uint64
 	hashInitOnce sync.Once
 )
 
 func generateHashSeed() {
-	now := time.Now()
-	seed := uint64(now.UnixNano())
-	seed ^= uint64(now.Unix()) << 32
-	seed ^= uint64(now.Nanosecond()) << 16
-	seed ^= uint64(uintptr(unsafe.Pointer(&seed)))
-	seed ^= uint64(uintptr(unsafe.Pointer(&now)))
-	hashSeed = uint32(seed)
+	seedBytes := make([]byte, 8)
+	n, err := rand.Read(seedBytes)
+	if err == nil && n == 8 {
+		hashSeed = binary.LittleEndian.Uint64(seedBytes)
+	} else {
+		now := time.Now()
+		seed := uint64(now.UnixNano())
+		seed ^= uint64(now.Unix()) << 32
+		seed ^= uint64(now.Nanosecond()) << 16
+		seed ^= uint64(uintptr(unsafe.Pointer(&seed)))
+		seed ^= uint64(uintptr(unsafe.Pointer(&now)))
+		hashSeed = seed
+	}
+	hashSeed ^= uint64(os.Getpid()) << 24
+	hashSeed ^= uint64(runtime.NumGoroutine()) << 8
+	var stackVar int
+	hashSeed ^= uint64(uintptr(unsafe.Pointer(&stackVar))) >> 3
+	mem := make([]byte, 1)
+	hashSeed ^= uint64(uintptr(unsafe.Pointer(&mem[0]))) >> 4
 }
 
 func initHashSeed() {
 	hashInitOnce.Do(generateHashSeed)
 }
 
-func CustomHash(buffer []byte) uint32 {
+func sha256Hash(buffer []byte) uint32 {
 	initHashSeed()
-	var h uint32 = hashSeed
-	for _, b := range buffer {
-		if b == 0 {
-			continue
+	hasher := sha256.New()
+	seedBytes := make([]byte, 8)
+	binary.LittleEndian.PutUint64(seedBytes, hashSeed)
+	hasher.Write(seedBytes)
+	runtimeBytes := make([]byte, 8)
+	binary.LittleEndian.PutUint64(runtimeBytes, uint64(time.Now().UnixNano()))
+	hasher.Write(runtimeBytes)
+	pidBytes := make([]byte, 4)
+	binary.LittleEndian.PutUint32(pidBytes, uint32(os.Getpid()))
+	hasher.Write(pidBytes)
+	upperBuffer := make([]byte, len(buffer))
+	for i, b := range buffer {
+		if b >= 'a' && b <= 'z' {
+			upperBuffer[i] = b - 0x20
+		} else {
+			upperBuffer[i] = b
 		}
-		if b >= 'a' {
-			b -= 0x20
-		}
-		h = (h ^ uint32(b)) * 16777619
 	}
-	return h
+	hasher.Write(upperBuffer)
+	hash := hasher.Sum(nil)
+	return binary.LittleEndian.Uint32(hash[:4])
 }
-
 var HashCache = make(map[string]uint32)
 var hashCacheMutex sync.RWMutex
 var collisionDetector = make(map[uint32]string)
@@ -121,7 +150,7 @@ func GetHash(s string) uint32 {
 	}
 	hashCacheMutex.RUnlock()
 
-	hash := CustomHash([]byte(s))
+	hash := sha256Hash([]byte(s))
 
 	hashCacheMutex.Lock()
 	HashCache[s] = hash
@@ -141,9 +170,9 @@ func detectHashCollision(hash uint32, newString string) {
 		normalizedExisting := strings.ToUpper(existingString)
 		if normalizedExisting != normalizedNew {
 			log.Printf("Warning: Hash collision detected!")
-			log.Printf("  Hash:", hash)
-			log.Printf("  Existing string:", existingString)
-			log.Printf("  New string:", newString)
+			log.Printf("  Hash: %d", hash)
+			log.Printf("  Existing string: %s", existingString)
+			log.Printf("  New string: %s", newString)
 		}
 	} else {
 		collisionDetector[hash] = newString
@@ -151,7 +180,7 @@ func detectHashCollision(hash uint32, newString string) {
 }
 
 func GetHashWithAlgorithm(s string, algorithm string) uint32 {
-	return CustomHash([]byte(s))
+	return sha256Hash([]byte(s))
 }
 
 func ClearHashCache() {
